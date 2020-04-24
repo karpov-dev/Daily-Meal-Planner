8d8b7301f1a9d56596a3b60b09970f1396530bf1
karpov-dev <54440209+karpov-dev@users.noreply.github.com>
2020-04-24 15:43:02 +0300

    Добавьте файлы проекта.

---
+++ Service Layer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Service_Layer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
